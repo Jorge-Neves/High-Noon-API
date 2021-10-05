745d9c9a2ae14d3d2f096c4a2ee882f6afc1bcb5
Jorge-Neves <paulo.jorge.ngs@gmail.com>
2021-10-05 17:47:59 +0100

    inc and dec fix

--- routes/skills.js
+++ routes/skills.js
@@ -37,6 +37,34 @@ router.delete("/skills/:id", async (req, res) => {
   }
 });
 
+router.post("/skills/:id/inc", async (req, res) => {
+  try {
+    await Skill.findByIdAndUpdate(req.params.id,  {
+      $inc: { timeSpent: 1}
+    });
+
+    res
+      .status(200)
+      .json({ message: `Skill with id ${req.params.id}s timeSpent was incremented.` });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
+
+router.post("/skills/:id/dec", async (req, res) => {
+  try {
+    await Skill.findByIdAndUpdate(req.params.id,  {
+      $inc: { timeSpent: -1}
+    });
+
+    res
+      .status(200)
+      .json({ message: `Skill with id ${req.params.id}s timeSpent was decremented.` });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
+
 router.get("/skills/:id", async (req, res) => {
   try {
     const response = await Skill.findById(req.params.id);
--- routes/tasks.js
+++ routes/tasks.js
@@ -37,6 +37,34 @@ router.delete("/tasks/:id", async (req, res) => {
   }
 });
 
+router.post("/tasks/:id/inc", async (req, res) => {
+  try {
+    await Task.findByIdAndUpdate(req.params.id,  {
+      $inc: { timeSpent: 1}
+    });
+
+    res
+      .status(200)
+      .json({ message: `Task with id ${req.params.id}s timeSpent was incremented.` });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
+
+router.post("/tasks/:id/dec", async (req, res) => {
+  try {
+    await Task.findByIdAndUpdate(req.params.id,  {
+      $inc: { timeSpent: -1}
+    });
+
+    res
+      .status(200)
+      .json({ message: `Task with id ${req.params.id}s timeSpent was decremented.` });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
+
 router.get("/tasks/:id", async (req, res) => {
   try {
     const response = await Task.findById(req.params.id);
